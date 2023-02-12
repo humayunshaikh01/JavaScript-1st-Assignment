79ff1ec16024f40a3761643aa1e95b69e2919a0f
humayunshaikh01 <shaikhhumayun99@gmail.com>
2023-02-12 22:23:00 +0500

    JavaScript 1st Assignment

---
+++ app.js
@@ -0,0 +1,81 @@
+// alert ("hellow")
+
+
+// Question No 1
+
+// var userAge = 20;
+
+// if(userAge == 20){
+//     console.log("User Allowed")
+// }else("User Not Allowed");
+
+// Question No 2
+
+// var UserGender = "male";
+
+// if(UserGender == "male"){
+//     console.log("user allow");
+// }else console.log("user not allowed");
+
+// Question No 3
+
+// var userGender = +prompt("Enter Your Age");
+
+// if(userGender == 18){
+//     console.log("user allow");
+// }else{
+//     console.log("user not allowed");
+// }
+
+// Question No 4
+
+// var userAge = prompt("Enter Your Age");
+
+// if(userAge > 20){
+//     console.log("user allow");
+// }else{
+//     console.log("user not allowed");
+// }
+
+
+// Question No 5
+
+// var userAge = prompt("Enter Your Age");
+
+// if(userAge >= 20){
+//     console.log("user is allowed");
+// }else{
+//     console.log("User not Allowed");
+// }
+
+
+// Question No 6
+
+// var userAge = prompt("Enter Your Age");
+
+// if(userAge >= 18 && userAge <= 40){
+//     console.log("User  Allowed")
+// }else{
+//     console.log("User not Allowed")
+// }
+
+// Question No 7
+
+// var userAge = prompt("Enter Your Age");
+
+// if(userAge >= 18 || userAge <= 40){
+//     console.log("User Allowed")
+// }else{
+//     console.log("User Not Allowed")
+// }
+
+// Question No 8
+
+// var userAge = prompt("Enter Your Age");
+// var userGender = prompt("Enter Your Gender")
+
+// if(userAge >= 18 && (userGender == "male" || userGender == "female")){
+//     console.log("User allowed");
+// }else{
+//     console.log("User Not allowed");
+// }
